6ab383b50f5bb95c0eb204571a7c1abfc383a95d
Dave Augustus <davetweetlive@gmail.com>
2020-03-31 02:41:08 +0530

    Removed everything to rewrite

--- main.go
+++ main.go
@@ -1,20 +1,4 @@
 package main
 
-import (
-	"log"
-	"os"
-	"text/template"
-)
-
-var tpl *template.Template
-
-func init() {
-	tpl = template.Must(template.ParseFiles("templates/index.html"))
-}
 func main() {
-
-	err := tpl.ExecuteTemplate(os.Stdout, "index.html", 42)
-	if err != nil {
-		log.Fatal(err)
-	}
 }
