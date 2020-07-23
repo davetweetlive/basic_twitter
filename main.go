5a8f414c521035d79cfe02b17e3cc710484626f2
Dave Augustus <davetweetlive@gmail.com>
2020-07-23 20:18:30 +0530

    A

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
-var (
-	name   string
-	course string
-	module float32
-)
-
 func main() {
-	// To get Os username
-	name = os.Getenv("USERNAME")
-	fmt.Println(name)
 
-	// os environment variable
-	for _, env := range os.Environ() {
-		fmt.Println(env)
-	}
 }
