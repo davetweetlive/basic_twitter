cde6ae878ff6d21830cd9084ebe851e9ada76b31
Dave Augustus <davetweetlive@gmail.com>
2020-07-28 17:21:18 +0530

    Authentication

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var client *redis.Client
@@ -25,7 +26,9 @@ func main() {
 	r.HandleFunc("/", indexPostHandler).Methods("POST")
 	r.HandleFunc("/login", loginGetHandler).Methods("GET")
 	r.HandleFunc("/login", loginPostHandler).Methods("POST")
-	r.HandleFunc("/test", testGetHandler).Methods("GET")
+	r.HandleFunc("/register", registerGetHandler).Methods("GET")
+	r.HandleFunc("/register", registerPostHandler).Methods("POST")
+
 	fs := http.FileServer(http.Dir("./static/"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 	http.Handle("/", r)
@@ -33,6 +36,13 @@ func main() {
 }
 
 func indexGetHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	_, ok := session.Values["username"]
+	if !ok {
+		http.Redirect(w, r, "/login", 302)
+		return
+	}
+
 	ctx := context.TODO()
 	comments, err := client.LRange(ctx, "comments", 0, 10).Result()
 	if err != nil {
@@ -55,22 +65,51 @@ func loginGetHandler(w http.ResponseWriter, r *http.Request) {
 
 func loginPostHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
+	ctx := context.TODO()
 	username := r.PostForm.Get("username")
+	password := r.PostForm.Get("password")
+	hash, err := client.Get(ctx, "user:"+username).Bytes()
+	if err != nil {
+		return
+	}
+	if err := bcrypt.CompareHashAndPassword(hash, []byte(password)); err != nil {
+		return
+	}
 	session, _ := store.Get(r, "session")
 	session.Values["username"] = username
 	session.Save(r, w)
 	http.Redirect(w, r, "/", 302)
 }
-func testGetHandler(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "session")
-	untype, ok := session.Values["username"]
 
-	if !ok {
-		return
-	}
-	username, ok := untype.(string)
-	if !ok {
-		return
-	}
-	w.Write([]byte(username))
+func registerGetHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "register.html", nil)
 }
+
+func registerPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	ctx := context.TODO()
+	username := r.PostForm.Get("username")
+	password := r.PostForm.Get("password")
+	cost := bcrypt.DefaultCost
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	if err != nil {
+		return
+	}
+	client.Set(ctx, "user:"+username, hash, 0)
+	http.Redirect(w, r, "/login", 302)
+}
+
+// func testGetHandler(w http.ResponseWriter, r *http.Request) {
+// 	session, _ := store.Get(r, "session")
+// 	untype, ok := session.Values["username"]
+
+// 	if !ok {
+// 		return
+// 	}
+// 	username, ok := untype.(string)
+// 	if !ok {
+// 		return
+// 	}
+// 	w.Write([]byte(username))
+// }
