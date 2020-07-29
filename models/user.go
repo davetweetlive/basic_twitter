94af728a9e362813935a4fec23a09062fddf2268
Dave Augustus <davetweetlive@gmail.com>
2020-07-30 00:32:46 +0530

    Worked till post correction and database restructure

--- models/user.go
+++ models/user.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/go-redis/redis"
 	"golang.org/x/crypto/bcrypt"
@@ -12,31 +13,85 @@ var (
 	ErrInvalidLogin = errors.New("Invalid Login!")
 )
 
+type User struct {
+	key string
+}
+
+func NewUser(username string, hash []byte) (*User, error) {
+	id, err := client.Incr(ctx, "user:next-id").Result()
+	if err != nil {
+		return nil, err
+	}
+	key := fmt.Sprintf("user:%d", id)
+	pipe := client.Pipeline()
+	pipe.HSet(ctx, key, "id", id)
+	pipe.HSet(ctx, key, "username", username)
+	pipe.HSet(ctx, key, "hash", hash)
+	pipe.HSet(ctx, "user:by-username", username, id)
+
+	_, err = pipe.Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &User{key}, nil
+}
+
+func (user *User) GetId() (int64, error) {
+	return client.HGet(ctx, user.key, "id").Int64()
+}
+
+func (user *User) GetUsername() (string, error) {
+	return client.HGet(ctx, user.key, "username").Result()
+}
+
+func (user *User) GetHash() ([]byte, error) {
+	return client.HGet(ctx, user.key, "hash").Bytes()
+}
+
+func (user *User) Authenticate(password string) error {
+	hash, err := user.GetHash()
+	if err != nil {
+		return err
+	}
+	err = bcrypt.CompareHashAndPassword(hash, []byte(password))
+	if err == bcrypt.ErrMismatchedHashAndPassword {
+		return ErrInvalidLogin
+	}
+	return err
+}
+
+func GetUserById(id int64) (*User, error) {
+	key := fmt.Sprintf("user:%d", id)
+	return &User{key}, nil
+}
+
+func GetUserByUsername(username string) (*User, error) {
+	id, err := client.HGet(ctx, "user:by-username", username).Int64()
+	if err == redis.Nil {
+
+		return nil, ErrUserNotFound
+	} else if err != nil {
+
+		return nil, err
+	}
+	return GetUserById(id)
+
+}
+
 func RegisterUser(username, password string) error {
 	cost := bcrypt.DefaultCost
-
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
 	if err != nil {
 		return err
 	}
-
-	return client.Set(ctx, "user:"+username, hash, 0).Err()
+	_, err = NewUser(username, hash)
+	return err
 }
 
-func AuthenticateUser(username, password string) error {
-	hash, err := client.Get(ctx, "user:"+username).Bytes()
-	if err == redis.Nil {
-
-		return ErrUserNotFound
-	} else if err != nil {
-
-		return err
-	}
-
-	err = bcrypt.CompareHashAndPassword(hash, []byte(password))
+func AuthenticateUser(username, password string) (*User, error) {
+	user, err := GetUserByUsername(username)
 	if err != nil {
-
-		return ErrInvalidLogin
+		return nil, err
 	}
-	return nil
+	return user, user.Authenticate(password)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -26,25 +26,31 @@ func NewRouter() *mux.Router {
 }
 
 func indexGetHandler(w http.ResponseWriter, r *http.Request) {
-	comments, err := models.GetComments()
+	updates, err := models.GetUpdates()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Internal server error!"))
 		return
 	}
-	utils.ExecuteTemplate(w, "index.html", comments)
+	utils.ExecuteTemplate(w, "index.html", updates)
 }
 
 func indexPostHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-
-	comment := r.PostForm.Get("comment")
-	if err := models.PostComment(comment); err != nil {
+	session, _ := sessions.Store.Get(r, "session")
+	untypedUserId := session.Values["user_id"]
+	userId, ok := untypedUserId.(int64)
+	if !ok {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Internal server error!"))
 		return
 	}
-
+	r.ParseForm()
+	body := r.PostForm.Get("update")
+	if err := models.PostUpdate(userId, body); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error!"))
+		return
+	}
 	http.Redirect(w, r, "/", 302)
 }
 
@@ -57,7 +63,7 @@ func loginPostHandler(w http.ResponseWriter, r *http.Request) {
 	username := r.PostForm.Get("username")
 	password := r.PostForm.Get("password")
 
-	err := models.AuthenticateUser(username, password)
+	user, err := models.AuthenticateUser(username, password)
 	if err != nil {
 		switch err {
 		case models.ErrUserNotFound:
@@ -71,8 +77,14 @@ func loginPostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	userId, err := user.GetId()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error!"))
+		return
+	}
 	session, _ := sessions.Store.Get(r, "session")
-	session.Values["username"] = username
+	session.Values["user_id"] = userId
 	session.Save(r, w)
 	http.Redirect(w, r, "/", 302)
 }
